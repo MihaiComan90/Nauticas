366e1a8c81b6d851ffcc7647280cff501044148c
Mihai Coman <mcoman@optaros.com>
2019-03-15 18:33:56 +0200

    varianta limba romana

--- admin/language/romana/module/product_variant.php
+++ admin/language/romana/module/product_variant.php
@@ -1,17 +1,17 @@
 <?php
 // Heading
-$_['heading_title']          = 'Product Variants';
+$_['heading_title']          = 'Variante de produs';
 
 /* Variants section*/
-$_['entry_attribute_option'] = 'Attribute Option';
-$_['option_variant_label']   = 'Mark as variant of this product';
-$_['option_variant_customurl_label'] = 'Custom url for your variant';
-$_['option_variant_customimage_label'] = 'Custom image for your variant';
-$_['option_variant_customprice_label'] = 'Custom price for your variant';
+$_['entry_attribute_option'] = 'Optiuni varianta';
+$_['option_variant_label']   = 'Marcheaza atributul ca varianta';
+$_['option_variant_customurl_label'] = 'URL-ul variantei';
+$_['option_variant_customimage_label'] = 'Imagine varianta';
+$_['option_variant_customprice_label'] = 'Pretul variante';
 /**/
-$_['text_success'] = 'Module setting successfully updated';
-$_['option_label_disable'] = 'Disabled';
-$_['enable_module_label'] = 'Module is';
-$_['option_label_enable'] = 'Enabled';
-$_['column_variant'] = 'Variant';
-$_['variant_label_none'] = 'No variant';
+$_['text_success'] = 'Setari actualizate';
+$_['option_label_disable'] = 'Inactiv';
+$_['enable_module_label'] = 'Modulul este';
+$_['option_label_enable'] = 'Activ';
+$_['column_variant'] = 'Varianta';
+$_['variant_label_none'] = 'Nici o varianta';
